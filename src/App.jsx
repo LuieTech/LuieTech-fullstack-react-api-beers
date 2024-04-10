7f2f21292c37a1fb764233f586f765ed564ae818
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-04-10 18:28:22 -0400

    chore:"implement Routes/Route to navigate pages"

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import AllBeersPage from "./pages/AllBeersPage"
+import RandomBeersPage from "./pages/RandomBeerPage"
+import BeerDetailsPage from "./pages/BeerDetailsPage"
+import AddBeerPage from "./pages/AddBeerPage"
+import Navbar from "./components/Navbar";
 
 function App() {
+
+  const location = useLocation()
+
   return (
     <div className="App">
-      <h1>LAB | React IronBeers</h1>
+      
+      {location.pathname !== "/" && <Navbar />}
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/beers" element={<AllBeersPage />} />
+          <Route path="/random-beer" element={<RandomBeersPage />} />
+          <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
+          <Route path="/new-beer" element={<AddBeerPage />} />
+        </Routes>
+      
     </div>
   );
 }
