961ca9108ec913e8ce2474606ef901cbf878e60d
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-04-10 18:29:37 -0400

    chore:"implement navbar componenr and css file"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,18 @@
-function Navbar() {}
+import { Link } from "react-router-dom";
+import "./navbar.css"
+import homeIcon from "../assets/home-icon.png"
+
+
+function Navbar() {
+
+  return <section className="navbar">
+  
+    <Link to={"/"}>
+      <img src={homeIcon} alt="home-icon" />
+    </Link>
+
+  </section>
+
+}
 
 export default Navbar;
